cc153f2b88ec2c7770c246a3ce601ab6d62c0f43
Abhishek N Mokashi <abhishekmokashi0102@gmail.com>
2024-07-01 22:10:07 +0530

    Update ReverseInteger_leetcode_snippet.java
    Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
    
    Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
    
     
    
    Example 1:
    
    Input: x = 123
    Output: 321
    Example 2:
    
    Input: x = -123
    Output: -321
    Example 3:
    
    Input: x = 120
    Output: 21
     
    
    Constraints:
    
    -231 <= x <= 231 - 1

--- ReverseInteger_leetcode_snippet.java
+++ ReverseInteger_leetcode_snippet.java
@@ -3,7 +3,7 @@ class Solution {
         long temp=0;
         while(x!=0)
         {
-            if(temp*10>Integer.MAX_VALUE || temp*10<Integer.MIN_VALUE)
+            if(temp*10>Integer.MAX_VALUE || temp*10<Integer.MIN_VALUE)  //Checking the constraint that integer value shoud be in range of -2147483648 to 2147483647 
             return 0;
             temp=(temp*10)+(x%10);
             x/=10;
